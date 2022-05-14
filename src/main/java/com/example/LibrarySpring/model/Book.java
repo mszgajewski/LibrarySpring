bcb469c9dd5d21e94f61610eb90a1015131c004c
mszgajewski <mszgajewski@gmail.com>
2022-05-14 21:13:07 +0200

    Initial Commit

--- src/main/java/com/example/LibrarySpring/model/Book.java
+++ src/main/java/com/example/LibrarySpring/model/Book.java
@@ -1,6 +1,8 @@
 package com.example.LibrarySpring.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,6 +10,8 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "Books")
 public class Book {
 
@@ -18,4 +22,9 @@ public class Book {
     private String author;
     private int isbn;
 
+    public Book(String title, String author, int isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
