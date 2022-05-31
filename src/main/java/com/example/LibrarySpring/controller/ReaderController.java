cb8c01cf555185d61bd542f3f9f107e020e45d74
mszgajewski <mszgajewski@gmail.com>
2022-05-31 22:53:58 +0200

    Initial Commit

--- src/main/java/com/example/LibrarySpring/controller/ReaderController.java
+++ src/main/java/com/example/LibrarySpring/controller/ReaderController.java
@@ -1,12 +1,14 @@
 package com.example.LibrarySpring.controller;
 
+import com.example.LibrarySpring.model.Book;
 import com.example.LibrarySpring.model.Reader;
 import com.example.LibrarySpring.service.ReaderService;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class ReaderController {
@@ -17,16 +19,42 @@ public class ReaderController {
         this.readerService = readerService;
     }
 
-    @GetMapping("/addReader")
+    @GetMapping("/addUser")
     public String addReader(Model model) {
         Reader reader = new Reader();
         model.addAttribute("reader", reader);
         return "register_form_user";
     }
 
-    @PostMapping("/addReader")
+    @PostMapping("/addUser")
     public String submitForm(@ModelAttribute("reader")Reader reader) {
         readerService.addReader(reader);
-        return "register_form_user";
+        return "user_register_success";
+    }
+
+    @PostMapping("/getUser")
+    public String deleteForm(@ModelAttribute("reader") Reader reader) {
+        readerService.addReader(reader);
+        return "user_register_success";
+    }
+
+    @RequestMapping("/allReaders")
+    public String showAllReaders(Model model) {
+        List<Reader> readers = readerService.findAll();
+        model.addAttribute("reader", readers);
+        return "list_of_readers";
+    }
+
+    @RequestMapping("/allReadersByName")
+    public String showAllReadersByName(Model model, @Param("name") String name) {
+        List<Reader> readers = readerService.findReaderByName(name);
+        model.addAttribute("reader", readers);
+        return "list_of_readers";
+    }
+
+    @DeleteMapping("/deleteReader")
+    public String deleteReader(@ModelAttribute("reader") Reader reader){
+        readerService.deleteReader(reader);
+        return "list_of_readers";
     }
 }
